d05f871c9b1dc39582a28179df41934534606cee
unpoomdoro <chi.nguyen@monstermedia.cz>
2022-06-10 10:26:31 +0200

    splitrequest  fix

--- src/main/java/cz/cvut/fit/splitee/controller/SplitRequest.java
+++ src/main/java/cz/cvut/fit/splitee/controller/SplitRequest.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 
 @AllArgsConstructor
+@NoArgsConstructor
 final class SplitObject {
     Long memberId;
     BigDecimal value;
@@ -14,10 +15,24 @@ final class SplitObject {
 }
 
 @AllArgsConstructor
+@NoArgsConstructor
 public class SplitRequest {
     Long billId;
     Long payerId;
     BigDecimal amount; //amount paid by payer. It is separate because payer might not be participant in a bill share
     TYPE type;
     ArrayList<SplitObject> splits;
+
+    public SplitRequest(Long billId, Long payerId, BigDecimal amount, String type, ArrayList<SplitObject> splits) {
+        this.billId = billId;
+        this.payerId = payerId;
+        this.amount = amount;
+        this.splits = splits;
+        switch (type) {
+            case "EQUAL" -> this.type = TYPE.EQUAL;
+            case "EXACT" -> this.type = TYPE.EXACT;
+            case "PERCENTAGE" -> this.type = TYPE.PERCENTAGE;
+            case "SHARE" -> this.type = TYPE.SHARE;
+        }
+    }
 }
