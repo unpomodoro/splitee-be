cbc329eb7eeb16cbbaed4ff68ea5f896993eb503
unpoomdoro <chi.nguyen@monstermedia.cz>
2022-06-10 23:31:26 +0200

    fix bill create

--- src/main/java/cz/cvut/fit/splitee/controller/SplitRequest.java
+++ src/main/java/cz/cvut/fit/splitee/controller/SplitRequest.java
@@ -2,19 +2,31 @@ package cz.cvut.fit.splitee.controller;
 
 import cz.cvut.fit.splitee.helper.TYPE;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 final class SplitObject {
     Long memberId;
     BigDecimal value;
     BigDecimal amount;
+
+    @Override
+    public String toString() {
+        return "SplitObject{" +
+                "memberId=" + memberId +
+                ", value=" + value +
+                ", amount=" + amount +
+                '}';
+    }
 }
 
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class SplitRequest {
@@ -23,6 +35,17 @@ public class SplitRequest {
     BigDecimal amount; //amount paid by payer. It is separate because payer might not be participant in a bill share
     TYPE type;
     ArrayList<SplitObject> splits;
+
+    @Override
+    public String toString() {
+        return "SplitRequest{" +
+                "billId=" + billId +
+                ", payerId=" + payerId +
+                ", amount=" + amount +
+                ", type=" + type +
+                ", splits=" + splits +
+                '}';
+    }
 
     public SplitRequest(Long billId, Long payerId, BigDecimal amount, String type, ArrayList<SplitObject> splits) {
         this.billId = billId;
